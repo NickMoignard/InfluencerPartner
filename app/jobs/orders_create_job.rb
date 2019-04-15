0708388695291f06e3a26117463aa9a4f82c2c40
NickMoignard <nicklmoignard@gmail.com>
2019-04-15 15:52:51 +1000

    dotenv not working atm temp - apikey and secret used

---
+++ app/jobs/orders_create_job.rb
@@ -0,0 +1,9 @@
+class OrdersCreateJob < ActiveJob::Base
+  def perform(shop_domain:, webhook:)
+    shop = Shop.find_by(shopify_domain: shop_domain)
+
+    shop.with_shopify_session do
+      puts "dad"
+    end
+  end
+end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,10 +1,14 @@
 ShopifyApp.configure do |config|
   config.application_name = "Influencer Partner"
-  config.api_key = ENV['API_KEY']
-  config.secret = ENV['SECRET']
+  config.api_key = "0506d033f96cd1a2ecc6bd074d61b651"
+  config.secret = "da336db92f9eff48a9df56b97b087d8e"
   config.scope = "read_orders, read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
+
+  config.webhooks = [
+    {topic: 'orders/create', address: 'https://influencer-partner.ngrok.io/webhooks/orders_create', format: 'json'},
+  ]
 end
