f98d0732f0c6a772c9d851e1f047e4f5643dd69c
NickMoignard <nicklmoignard@gmail.com>
2019-04-17 12:52:44 +1000

    added helper function to total creator orders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,9 +8,13 @@ class HomeController < ShopifyApp::AuthenticatedController
     Creator.all.each do |c|
       @orders[c.code] = Order.where(:creator => c).where(:paid => false)
     end
+
+    creator_orders_value(@creators.first)
+    
   end
 
   def payout
+    #RAKIBUL
     if params[:code].nil? 
       puts 'Code parameter is nil'
     else
@@ -18,5 +22,25 @@ class HomeController < ShopifyApp::AuthenticatedController
       PayoutCreatorJob.perform_now(creator)
     end
   end
+
+  private
+
+  def creator_orders_value(creator)
+    #RAKIBUL
+
+    creator_orders = Order.where(creator: creator)
+    store_orders = []
+    total_price_usd = 0.0
+
+    creator_orders.each do |o|
+      store_orders.append(ShopifyAPI::Order.find(o.order_id))
+    end
+    store_orders.each do |o|
+      total_price_usd = total_price_usd + o.total_price_usd.to_f
+    end
+
+    return total_price_usd
+  end
+
 
 end
